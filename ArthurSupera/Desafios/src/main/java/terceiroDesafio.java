92071a4d2d149b76dcc8ff748de17350c239f47e
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2023-07-12 09:42:56 -0300

    Update terceiroDesafio.java
    now working

--- ArthurSupera/Desafios/src/main/java/terceiroDesafio.java
+++ ArthurSupera/Desafios/src/main/java/terceiroDesafio.java
@@ -1,29 +1,40 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class terceiroDesafio {
-    public static void main(String[] args) {
-        int[] arr = {1, 5, 3, 4, 2};
-        int k = 2;
-        System.out.println(countPairsWithDiffK(arr, k));
+    public static int countPairsWithDiff(int[] arr, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int num : arr) {
+            int target1 = num - k;
+            int target2 = num + k;
+
+            if (set.contains(target1)) {
+                count++;
+            }
+
+            if (set.contains(target2)) {
+                count++;
+            }
+
+            set.add(num);
+        }
+
+        return count;
     }
 
-    static int countPairsWithDiffK(int arr[], int k) {
-        int n = arr.length;
-        Arrays.sort(arr);
-        int count = 0;
-        int l = 0;
-        int r = 0;
-        while (r < n) {
-            if (arr[r] - arr[l] == k) {
-                count++;
-                l++;
-                r++;
-            } else if (arr[r] - arr[l] > k) {
-                l++;
-            } else { // arr[r] - arr[l] < k
-                r++;
-            }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // tamanho do array
+        int k = scanner.nextInt(); // valor alvo
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt(); // elementos do array
         }
-        return count;
+
+        int result = countPairsWithDiff(arr, k);
+        System.out.println(result);
     }
 }
