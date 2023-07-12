5c5c7414e97b3652966b8f812c218e7a0c8041e5
ArthurTB8 <107354840+ArthurTB8@users.noreply.github.com>
2023-07-12 10:09:06 -0300

    Update quartoDesafio.java

--- ArthurSupera/Desafios/src/main/java/quartoDesafio.java
+++ ArthurSupera/Desafios/src/main/java/quartoDesafio.java
@@ -8,11 +8,15 @@ public class quartoDesafio {
 
         for (int i = 0; i < n; i++) {
             String linha = scanner.nextLine();
+            
             int metadeDoTamanho = linha.length() / 2;
+            
             String metadeEsquerda = linha.substring(0, metadeDoTamanho);
             String metadeDireita = linha.substring(metadeDoTamanho);
+            
             String metadeEsquerdaInvertida = new StringBuilder(metadeEsquerda).reverse().toString();
             String metadeDireitaInvertida = new StringBuilder(metadeDireita).reverse().toString();
+            
             System.out.println(metadeEsquerdaInvertida + metadeDireitaInvertida);
         }
     }
